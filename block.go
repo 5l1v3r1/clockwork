a3df14aa2b85f2afde980d9e82eef690e71e09ef
Alex Nichol <unixpickle@gmail.com>
2016-12-20 12:08:21 -0500

    fixed serialization bug & drafted ApplyBlock

--- block.go
+++ block.go
@@ -33,12 +33,14 @@ type blockRState struct {
 // Block is an rnn.Block which implements the traditional
 // Clockwork RNN architecture.
 type Block struct {
-	stateTransformers denseList         `json:"-"`
-	inputTransformers denseList         `json:"-"`
-	squasher          neuralnet.Network `json:"-"`
-	frequencies       []int
-	initState         *autofunc.Variable
-	fullyConnected    bool
+	stateTransformers denseList
+	inputTransformers denseList
+	squasher          neuralnet.Network
+	metadata          struct {
+		Frequencies    []int
+		InitState      *autofunc.Variable
+		FullyConnected bool
+	}
 }
 
 // DeserializeBlock deserializes a block.
@@ -50,7 +52,7 @@ func DeserializeBlock(d []byte) (*Block, error) {
 	if err != nil {
 		return nil, err
 	}
-	if err := json.Unmarshal(jsonData, &res); err != nil {
+	if err := json.Unmarshal(jsonData, &res.metadata); err != nil {
 		return nil, err
 	}
 	return &res, nil
@@ -76,8 +78,8 @@ func NewBlock(inSize int, freqs []int, stateSizes []int) *Block {
 	res.squasher = neuralnet.Network{
 		&neuralnet.HyperbolicTangent{},
 	}
-	res.frequencies = freqs
-	res.initState = &autofunc.Variable{
+	res.metadata.Frequencies = freqs
+	res.metadata.InitState = &autofunc.Variable{
 		Vector: make(linalg.Vector, totalState),
 	}
 	return res
@@ -105,11 +107,11 @@ func NewBlockFC(inSize int, freqs []int, stateSizes []int) *Block {
 	res.squasher = neuralnet.Network{
 		&neuralnet.HyperbolicTangent{},
 	}
-	res.frequencies = freqs
-	res.initState = &autofunc.Variable{
+	res.metadata.Frequencies = freqs
+	res.metadata.InitState = &autofunc.Variable{
 		Vector: make(linalg.Vector, totalState),
 	}
-	res.fullyConnected = true
+	res.metadata.FullyConnected = true
 	return res
 }
 
@@ -117,13 +119,13 @@ func NewBlockFC(inSize int, freqs []int, stateSizes []int) *Block {
 func (b *Block) StartState() rnn.State {
 	return &blockState{
 		timestep: 0,
-		states:   b.initState.Vector,
+		states:   b.metadata.InitState.Vector,
 	}
 }
 
 // StartRState returns the initial r-state.
 func (b *Block) StartRState(rv autofunc.RVector) rnn.RState {
-	v := autofunc.NewRVariable(b.initState, rv)
+	v := autofunc.NewRVariable(b.metadata.InitState, rv)
 	return &blockRState{
 		timestep: 0,
 		states:   v.Output(),
@@ -133,24 +135,67 @@ func (b *Block) StartRState(rv autofunc.RVector) rnn.RState {
 
 // PropagateStart propagates through the start state.
 func (b *Block) PropagateStart(s []rnn.State, u []rnn.StateGrad, g autofunc.Gradient) {
-	rnn.PropagateVarState(b.initState, u, g)
+	rnn.PropagateVarState(b.metadata.InitState, u, g)
 }
 
 // PropagateStartR propagates through the start state.
 func (b *Block) PropagateStartR(s []rnn.RState, u []rnn.RStateGrad,
 	rg autofunc.RGradient, g autofunc.Gradient) {
-	rnn.PropagateVarStateR(b.initState, u, rg, g)
+	rnn.PropagateVarStateR(b.metadata.InitState, u, rg, g)
+}
+
+// ApplyBlock applies the block.
+func (b *Block) ApplyBlock(s []rnn.State, in []autofunc.Result) rnn.BlockResult {
+	var statePool []*autofunc.Variable
+	var stateRes []autofunc.Result
+	for _, state := range s {
+		stateObj := state.(*blockState)
+		p := &autofunc.Variable{Vector: stateObj.states}
+		statePool = append(statePool, p)
+		stateRes = append(stateRes, p)
+	}
+
+	newStates := make([]autofunc.Result, len(b.metadata.Frequencies))
+	for i := range b.metadata.Frequencies {
+		newStates[i] = b.applySubBlock(i, s, stateRes, in)
+	}
+	joinedRes := b.weaveStates(len(in), newStates)
+
+	splitOut := autofunc.Split(len(in), joinedRes)
+	var outVecs []linalg.Vector
+	var outStates []rnn.State
+	for i, x := range splitOut {
+		outVecs = append(outVecs, x.Output())
+		outStates = append(outStates, &blockState{
+			states:   x.Output(),
+			timestep: s[i].(*blockState).timestep + 1,
+		})
+	}
+
+	return &blockResult{
+		StatePool: statePool,
+		Results:   joinedRes,
+		OutVecs:   outVecs,
+		OutStates: outStates,
+	}
+}
+
+// ApplyBlockR applies the block.
+func (b *Block) ApplyBlockR(v autofunc.RVector, s []rnn.RState,
+	in []autofunc.RResult) rnn.BlockRResult {
+	// TODO: this.
+	return nil
 }
 
 // Frequencies returns the frequencies of the internal
 // blocks, sorted from lowest to highest.
 func (b *Block) Frequencies() []int {
-	return append([]int{}, b.frequencies...)
+	return append([]int{}, b.metadata.Frequencies...)
 }
 
 // Parameters returns the parameters of the CWRNN.
 func (b *Block) Parameters() []*autofunc.Variable {
-	res := []*autofunc.Variable{b.initState}
+	res := []*autofunc.Variable{b.metadata.InitState}
 	res = append(res, b.stateTransformers.Parameters()...)
 	res = append(res, b.inputTransformers.Parameters()...)
 	return append(res, b.squasher.Parameters()...)
@@ -164,12 +209,93 @@ func (b *Block) SerializerType() string {
 
 // Serialize serializes a block.
 func (b *Block) Serialize() ([]byte, error) {
-	jsonData, err := json.Marshal(b)
+	jsonData, err := json.Marshal(&b.metadata)
 	if err != nil {
 		return nil, err
 	}
 	return serializer.SerializeAny(b.stateTransformers, b.inputTransformers,
 		b.squasher, serializer.Bytes(jsonData))
+}
+
+func (b *Block) applySubBlock(subIdx int, inState []rnn.State, pool []autofunc.Result,
+	in []autofunc.Result) autofunc.Result {
+	stateIdx := 0
+	for i := 0; i < subIdx; i++ {
+		stateIdx += b.stateTransformers[i].Weights.Rows
+	}
+	stateSize := b.stateTransformers[subIdx].Weights.Rows
+	freq := b.metadata.Frequencies[subIdx]
+
+	var transformStates []autofunc.Result
+	var transformIns []autofunc.Result
+	for i, s := range inState {
+		if s.(*blockState).timestep%freq == 0 {
+			transformIns = append(transformIns, in[i])
+			if b.metadata.FullyConnected {
+				transformStates = append(transformStates, pool[i])
+			} else {
+				inSize := b.stateTransformers[subIdx].Weights.Cols
+				slowerOrSame := autofunc.Slice(pool[i], stateIdx, inSize)
+				transformStates = append(transformStates, slowerOrSame)
+			}
+		}
+	}
+
+	if len(transformStates) == 0 {
+		var prevStates []autofunc.Result
+		for _, x := range pool {
+			p := autofunc.Slice(x, stateIdx, stateSize)
+			prevStates = append(prevStates, p)
+		}
+		return autofunc.Concat(prevStates...)
+	}
+
+	transformState := autofunc.Concat(transformStates...)
+	transformIn := autofunc.Concat(transformIns...)
+
+	newStates := b.squasher.Apply(
+		autofunc.Add(
+			b.stateTransformers[subIdx].Batch(transformState, len(transformIns)),
+			b.inputTransformers[subIdx].Batch(transformIn, len(transformIns)),
+		),
+	)
+	if len(transformStates) == len(in) {
+		return newStates
+	}
+
+	// Only necessary if some input states were on different
+	// timesteps than others.
+	return autofunc.Pool(newStates, func(newStates autofunc.Result) autofunc.Result {
+		split := autofunc.Split(len(transformStates), newStates)
+		var res []autofunc.Result
+		var idx int
+		for i, s := range inState {
+			if s.(*blockState).timestep%freq == 0 {
+				res = append(res, split[idx])
+				idx++
+			} else {
+				p := autofunc.Slice(pool[i], stateIdx, stateSize)
+				res = append(res, p)
+			}
+		}
+		return autofunc.Concat(res...)
+	})
+}
+
+func (b *Block) weaveStates(numIn int, subStates []autofunc.Result) autofunc.Result {
+	return autofunc.PoolAll(subStates, func(subStates []autofunc.Result) autofunc.Result {
+		splitSub := make([][]autofunc.Result, len(subStates))
+		for i, s := range subStates {
+			splitSub[i] = autofunc.Split(numIn, s)
+		}
+		var ordered []autofunc.Result
+		for i := 0; i < numIn; i++ {
+			for _, x := range splitSub {
+				ordered = append(ordered, x[i])
+			}
+		}
+		return autofunc.Concat(ordered...)
+	})
 }
 
 type blockResult struct {
